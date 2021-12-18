3ffd5b82a42ceb1cc0f47483568e4a2f44808d9e
Erickson Dutra <ericksond10@gmail.com>
2021-12-18 15:12:08 -0400

    .

--- agenda/urls.py
+++ agenda/urls.py
@@ -17,14 +17,18 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/agenda/')),
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+    path('agenda/lista/<int:id_usuario>/', views.json_lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
-    path('', RedirectView.as_view(url='/agenda/')),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     path('login/', views.login_user),
     path('logout', views.logout_user),
     path('login/submit', views.submit_login)
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import redirect, render
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
+from django.contrib.auth.models import User
 
 def login_user(request):
     return render(request, 'login.html')
@@ -26,13 +29,21 @@ def submit_login(request):
 
 @login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario,
+                                  data_evento__gt=data_atual)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    print(id_evento)
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
 @login_required(login_url='/login/')
 def submit_evento(request):
@@ -40,12 +51,39 @@ def submit_evento(request):
         titulo = request.POST.get('titulo')
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
+        local = request.POST.get('local')
         usuario = request.user
-        Evento.objects.create(
-            titulo = titulo,
-            data_evento = data_evento,
-            descricao = descricao,
-            usuario = usuario,
-        )
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            Evento.objects.filter(id=id_evento).update(titulo = titulo,
+                data_evento = data_evento,
+                descricao = descricao,
+                local = local)
+
+        else:
+            Evento.objects.create(
+                titulo = titulo,
+                data_evento = data_evento,
+                descricao = descricao,
+                usuario = usuario,
+                local = local,
+            )
     return redirect('/')
     
+@login_required(login_url='/login/')
+def delete_evento (request, id_evento):
+    usuario = request.user
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
+    if usuario == evento.usuario:
+        evento.delete()
+    else:
+        raise Http404()
+    return redirect('/')
+
+def json_lista_eventos(request, id_usuario):
+    usuario = User.objects.get(id=id_usuario)
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento), safe=False)
